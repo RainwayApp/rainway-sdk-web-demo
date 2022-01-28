9c9e7acd27f549a87d63d1f8c16168d2ff28fe73
Ben Greenier <ben@bengreenier.com>
2022-01-28 10:42:15 -0800

    chore(release): Update to `0.2.3` sdk
    Additionally, add small fixes for new naming

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -4,6 +4,7 @@ import {
     RainwayError,
     RainwayLogLevel,
     RainwayRuntime,
+    RainwayChannelMode,
     InputLevel,
 } from "rainway-sdk";
 
@@ -12,7 +13,7 @@ try {
     config = require("../local-config.json");
 } catch (e) {}
 
-let sandboxApiKey = localStorage.getItem("api-key") || config.apiKey || "NO_KEY";
+let sandboxApiKey = localStorage.getItem("api-key") ?? config.apiKey ?? "NO_KEY";
 const allInput = InputLevel.Keyboard | InputLevel.Mouse | InputLevel.Gamepad;
 
 const isDesktopSafari =
@@ -21,14 +22,15 @@ const isDesktopSafari =
 
 enum SandboxState {
     Disconnected,
-    ConnectingToRelay,
-    ConnectedToRelay,
+    ConnectingToGateway,
+    ConnectedToGateway,
 }
 
 enum SandboxWidgetState {
     Disconnected,
     ConnectingToHost,
     ConnectedToHostNoStream,
+    ConnectedToHostCantStream,
     ConnectedToHostReadyToStream,
     ConnectedToHostStreaming,
 }
@@ -37,14 +39,14 @@ class StreamWidget {
     peer: RainwayPeer | undefined;
     stream: RainwayStream | undefined;
     state: SandboxWidgetState = SandboxWidgetState.Disconnected;
-    hostname: HTMLInputElement;
+    peerId: HTMLInputElement;
     fullscreenButton: HTMLButtonElement;
+    toggleGesturesButton: HTMLButtonElement;
     pauseButton: HTMLButtonElement;
     statsButton: HTMLButtonElement;
     chatHistory: HTMLDivElement;
     chatInput: HTMLInputElement;
     chatSendButton: HTMLButtonElement;
-    
 
     private getElement = <T extends HTMLElement>(className: string) =>
         document.querySelector(`.${this.widgetClassName} .${className}`)! as T;
@@ -52,8 +54,9 @@ class StreamWidget {
     constructor(private runtime: RainwayRuntime, private widgetClassName: string) {
         // Bind some DOM elements and set up listeners
 
-        this.hostname = this.getElement("hostname");
+        this.peerId = this.getElement("peer-id");
         this.fullscreenButton = this.getElement("fullscreen-button");
+        this.toggleGesturesButton = this.getElement("toggle-gestures-button");
         this.pauseButton = this.getElement("pause-button");
         this.statsButton = this.getElement("stats-button");
         this.chatHistory = this.getElement("chat-history");
@@ -62,6 +65,15 @@ class StreamWidget {
 
         this.fullscreenButton.addEventListener("click", () => {
             this.stream?.requestFullscreen();
+        });
+        this.toggleGesturesButton.addEventListener("click", () => {
+            if (this.toggleGesturesButton.innerText === "Enable gestures") {
+                this.stream?.enableGestures();
+                this.toggleGesturesButton.innerText = "Disable gestures";
+            } else {
+                this.stream?.disableGestures();
+                this.toggleGesturesButton.innerText = "Enable gestures";
+            }
         });
         this.statsButton.addEventListener("click", () => {
             if (this.statsButton.innerText === "Show stats") {
@@ -103,15 +115,15 @@ class StreamWidget {
             this.stream?.leave();
         });
 
-        // Populate hostname from config, then localStorage, and finally default to "608f3a6b-0100-0000-0000-000000000000"
-        this.hostname.value =
-            config.hostname ??
-            localStorage.getItem("hostname-" + widgetClassName) ??
-            "000000000000000000";
+        // Populate peerId from config, then localStorage, and finally default to "555555555123456789"
+        this.peerId.value =
+            config.peerId ??
+            localStorage.getItem("peerId-" + widgetClassName) ??
+            "555555555123456789";
 
-        // Persist hostname to localStorage for convenience
-        this.hostname.addEventListener("change", () => {
-            localStorage.setItem("hostname-" + widgetClassName, this.hostname.value);
+        // Persist peerId to localStorage for convenience
+        this.peerId.addEventListener("change", () => {
+            localStorage.setItem("peerId-" + widgetClassName, this.peerId.value);
         });
 
         this.getElement("connect-to-host-button").addEventListener("click", (e) =>
@@ -126,7 +138,7 @@ class StreamWidget {
     /** Send UTF-8 encoded message to peer over arbitrary messaging channel and display in the chatbox. */
     private sendChatMessage(message: string): void {
         if (this.peer) {
-            this.peer.send(new TextEncoder().encode(message));
+            this.peer.send("Message", new TextEncoder().encode(message));
             this.displayChatMessage("You", message);
             this.chatInput.value = "";
         }
@@ -143,7 +155,7 @@ class StreamWidget {
     }
 
     /** Add an "info" message to the chatbox. */
-    private displayChatInfo(message: string): void {
+    public displayChatInfo(message: string): void {
         const p = document.createElement("p");
         p.appendChild(document.createTextNode(message));
         p.className = "chat-info";
@@ -157,9 +169,11 @@ class StreamWidget {
         }
     }
 
-    /** Short version of hostname. */
+    /** Human-readable nickname for this peer. */
     public hostNickname(): string {
-        return this.hostname.value.substring(29);
+        if (!this.peer) return "(no peer)";
+        if (!this.peer.externalId) return "(anonymous peer)";
+        return this.peer.externalId;
     }
 
     private showError(e: any): void {
@@ -167,25 +181,24 @@ class StreamWidget {
         this.getElement("rainway-client-state")!.innerText = error;
     }
 
-    /** Connect to a stream hosting-capable peer. Will connect parent runtime to instant relay first if not connected. */
+    /** Connect to a stream hosting-capable peer. Will connect parent runtime to Gateway first if not connected. */
     async connectToHost(): Promise<void> {
         if (this.runtime === undefined) throw new Error();
         await this.runtime.connectToGateway();
 
         this.setUIState(SandboxWidgetState.ConnectingToHost);
         try {
-            this.peer = await this.runtime.connect(BigInt(this.hostname.value));
+            this.peer = await this.runtime.connect(BigInt(this.peerId.value));
         } catch (e) {
             this.setUIState(SandboxWidgetState.Disconnected);
             this.showError(e);
             return;
         }
-        this.setUIState(SandboxWidgetState.ConnectedToHostNoStream);
-        this.displayChatInfo(`Connected to ${this.hostNickname()}. Say hi!`);
+        this.peer.createDataChannel("Message", RainwayChannelMode.Reliable);
 
-        await this.peer.readyToStream;
-        this.setUIState(SandboxWidgetState.ConnectedToHostReadyToStream);
-        this.displayChatInfo("Ready to stream.");
+        this.setUIState(SandboxWidgetState.ConnectedToHostNoStream);
+        this.displayChatInfo(`Connected to "${this.hostNickname()}". Say hi!`);
+        await this.awaitReadyToStream();
     }
 
     /** Disconnect from peer or cancel connection attempt in progress. */
@@ -194,7 +207,7 @@ class StreamWidget {
 
         this.setUIState(SandboxWidgetState.Disconnected);
         if (this.peer === undefined) {
-            this.runtime.cancelConnectionAttempt(BigInt(this.hostname.value));
+            this.runtime.cancelConnectionAttempt(BigInt(this.peerId.value));
             return;
         }
         this.peer.disconnect();
@@ -207,6 +220,7 @@ class StreamWidget {
             [S.Disconnected]: "Disconnected",
             [S.ConnectingToHost]: "Connecting to host...",
             [S.ConnectedToHostNoStream]: "Connected (preparing stream...)",
+            [S.ConnectedToHostCantStream]: "Connected (peer can't stream)",
             [S.ConnectedToHostReadyToStream]: "Connected (ready to stream)",
             [S.ConnectedToHostStreaming]: "Streaming",
         };
@@ -219,6 +233,24 @@ class StreamWidget {
         this.enableInputs("when-streaming", newState >= S.ConnectedToHostStreaming);
 
         this.state = newState;
+    }
+
+    public async awaitReadyToStream(): Promise<void> {
+        if (!this.peer) return;
+        if (await this.peer.readyToStream) {
+            this.setUIState(SandboxWidgetState.ConnectedToHostReadyToStream);
+            this.displayChatInfo("Ready to stream.");
+        } else {
+            this.setUIState(SandboxWidgetState.ConnectedToHostCantStream);
+            this.displayChatInfo(
+                "The remote peer can't stream media. You can still send chat messages back and forth.",
+            );
+            if (/web/.test(this.peer.externalId)) {
+                this.displayChatInfo(
+                    "(Did you connect two Web SDK demos? Rainway currently only supports streaming from the Windows 10 Native SDK to a web environment.)",
+                );
+            }
+        }
     }
 }
 
@@ -246,17 +278,19 @@ export class StreamSandbox {
             w.className = `widget widget${i}`;
             this.querySelector(".widgets").appendChild(w);
         }
-        ;(this.querySelector(".api-key") as HTMLInputElement).value = sandboxApiKey
+
+        this.querySelector<HTMLInputElement>(".api-key").value = sandboxApiKey;
+
         this.querySelector(".api-key").addEventListener("change", (event) => {
             localStorage.setItem("api-key", (event.target as HTMLInputElement).value)
             sandboxApiKey = (event.target as HTMLInputElement).value
-        })
+        });
 
-        this.querySelector(".connect-to-relay-button").addEventListener("click", () =>
-            this.connectToRelay(),
+        this.querySelector(".connect-to-gateway-button").addEventListener("click", () =>
+            this.connectToGateway(),
         );
-        this.querySelector(".disconnect-from-relay-button").addEventListener("click", () =>
-            this.disconnectFromRelay(),
+        this.querySelector(".disconnect-from-gateway-button").addEventListener("click", () =>
+            this.disconnectFromGateway(),
         );
 
         this.runtime = undefined;
@@ -268,6 +302,19 @@ export class StreamSandbox {
         this.logVideoStats = config.logVideoStats ?? false;
         this.logTransportStats = config.logTransportStats ?? false;
         this.rainwayLogsInConsole = config.rainwayLogsInConsole ?? true;
+    }
+
+    private addWidget(): StreamWidget {
+        const template = this.querySelector<HTMLTemplateElement>("template.widget");
+        const templateDiv = template.content.firstElementChild!;
+        const i = ++this.numWidgets;
+        const widgetDiv = templateDiv.cloneNode(true) as HTMLDivElement;
+        widgetDiv.className = `widget widget${i}`;
+        this.querySelector(".widgets").appendChild(widgetDiv);
+        const widget = new StreamWidget(this.runtime!, `widget${i}`);
+        this.widgets.push(widget);
+        this.querySelector(`.widget${i}`).hidden = false;
+        return widget;
     }
 
     /** Log to web console based on Rainway log level. */
@@ -296,35 +343,64 @@ export class StreamSandbox {
         }
     }
 
-    /** Instantiate runtime if not yet built, connect it to instant relay. */
-    async connectToRelay(): Promise<void> {
+    /** Instantiate runtime if not yet built, connect it to Gateway. */
+    async connectToGateway(): Promise<void> {
         // Make and attach the Rainway client.
-        this.setUIState(SandboxState.ConnectingToRelay);
+        this.setUIState(SandboxState.ConnectingToGateway);
         if (!this.runtime) {
             this.runtime = await RainwayRuntime.initialize({
                 apiKey: sandboxApiKey,
-                externalId: "web-sdk-demo-sandbox",
-                // Listener for instant relay connection loss
+                externalId: "web runtime sandbox",
+                // Listener for Gateway connection loss
                 onRuntimeConnectionLost: (error) => {
                     console.log("Connection lost:", error);
                     this.setUIState(SandboxState.Disconnected);
                 },
                 // Listener for incoming connection requests
                 onConnectionRequest: (request) => {
-                    // Auto-accept every request.
-                    request.accept();
+                    window.confirm(
+                        `Connection request from ${request.peerId} (${request.externalId}). Accept it?`,
+                    )
+                        ? request.accept()
+                        : request.reject("Rejected via web sandbox prompt");
                 },
                 // Listener for incoming arbitrary messages
-                onPeerMessage: (peer, data) => {
+                onPeerMessage: (peer, channelName, data) => {
                     // the only arbitrary messages we're listening for are basic chat messages
                     const w = this.widgets.find((w) => w.peer === peer);
                     if (w === undefined) return;
                     w.displayChatMessage(w.hostNickname(), new TextDecoder().decode(data));
                 },
+                // Listener for peer data channel creation
+                onPeerDataChannel: (peer, channelName, event) => {},
                 // Listener for errors from particular peers
                 onPeerError: (peer, error) => {},
                 // Listener for when a peer finishes connecting
-                onPeerConnect: (peer) => {},
+                onPeerConnect: async (peer) => {
+                    console.log("onPeerConnect", peer, this.widgets);
+
+                    // Do nothing if this peer already has a widget:
+                    if (
+                        this.widgets.some(
+                            (w) =>
+                                w.peer === peer ||
+                                (w.state === SandboxWidgetState.ConnectingToHost &&
+                                    w.peerId.value === peer.peerId.toString()),
+                        )
+                    )
+                        return;
+
+                    // Find or create a widget for this peer.
+                    const available = this.widgets.find(
+                        (w) => w.state === SandboxWidgetState.Disconnected,
+                    );
+                    const widget = available ?? this.addWidget();
+                    widget.peer = peer;
+                    widget.peerId.value = peer.peerId.toString();
+                    widget.displayChatInfo("Accepted a connection from " + peer.peerId);
+                    widget.setUIState(SandboxWidgetState.ConnectedToHostNoStream);
+                    await widget.awaitReadyToStream();
+                },
                 // Listener for peer disconnect
                 onPeerDisconnect: (peer) => {
                     // remove appropriate stream container when a peer disconnects
@@ -342,10 +418,10 @@ export class StreamSandbox {
                 // Listener for stream stop
                 onStreamStop: (stream) => {
                     // When the stream stops, remove the container from the DOM
-                    const w = this.widgets.find((w) => w.peer?.stream === stream);
+                    const w = this.widgets.find((w) => w.stream === stream);
                     if (w === undefined) return;
                     stream.container.remove();
-                    if (w.state > SandboxWidgetState.ConnectedToHostReadyToStream) {
+                    if (w.state === SandboxWidgetState.ConnectedToHostStreaming) {
                         w.setUIState(SandboxWidgetState.ConnectedToHostReadyToStream);
                     }
                 },
@@ -356,8 +432,8 @@ export class StreamSandbox {
         }
         // This call is not strictly necessary if the runtime was just constructed:
         await this.runtime.connectToGateway();
-        // The runtime automatically connects to instant relay when initialized.
-        this.setUIState(SandboxState.ConnectedToRelay);
+        // The runtime automatically connects to Gateway when initialized.
+        this.setUIState(SandboxState.ConnectedToGateway);
         if (this.widgets.length === 0) {
             for (let i = 1; i <= this.numWidgets; i++) {
                 this.widgets.push(new StreamWidget(this.runtime, `widget${i}`));
@@ -371,19 +447,19 @@ export class StreamSandbox {
         const S = SandboxState;
         const descriptions = {
             [S.Disconnected]: "Disconnected",
-            [S.ConnectingToRelay]: "Connecting...",
-            [S.ConnectedToRelay]: "Connected to Gateway as " + this.runtime?.getPeerId(),
+            [S.ConnectingToGateway]: "Connecting...",
+            [S.ConnectedToGateway]: "Connected to gateway as " + this.runtime?.getPeerId(),
         };
         this.querySelector(".rainway-outer-state")!.innerText = descriptions[newState];
 
-        this.enableInputs("when-no-relay", newState === S.Disconnected);
-        this.enableInputs("when-relay", newState >= S.ConnectedToRelay);
+        this.enableInputs("when-no-gateway", newState === S.Disconnected);
+        this.enableInputs("when-gateway", newState >= S.ConnectedToGateway);
 
         this.state = newState;
     }
 
-    /** Disconnect from instant relay. Runtime can be reconnected later. */
-    async disconnectFromRelay() {
+    /** Disconnect from Gateway. Runtime can be reconnected later. */
+    async disconnectFromGateway() {
         this.runtime?.disconnectFromGateway();
         this.setUIState(SandboxState.Disconnected);
     }
